8a75c98a801c86a651e3ee2badab16e9aa76dfaa
Poornima0716 <poornimanive@gmail.com>
2021-12-31 20:37:31 +0530

    Password Functionality

--- src/org/test/Sample.java
+++ src/org/test/Sample.java
@@ -1,5 +1,13 @@
 package org.test;
 
 public class Sample {
-
+	
+public static void password() {
+	System.out.println("pass");
+	
+}
+public static void main(String[] args) {
+	password();
+	
+}
 }
